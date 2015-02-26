48cbd6acc8ab299dc834a23ae5ababb2ab4f5e32
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-26 09:22:13 +0300

    ref/tests

---
+++ verification/src/tests.py
@@ -0,0 +1,21 @@
+TESTS = {
+    "Level_1": [
+        {"input": "18", "answer": 10},
+        {"input": "1010101011", "answer": 2},
+        {"input": "222", "answer": 3},
+        {"input": "A23B", "answer": 14},
+        {"input": "123F", "answer": 22},
+        {"input": "IDDQD", "answer": 0},
+        {"input": "ZZZ", "answer": 36},
+        {"input": "XYZ", "answer": 0},
+        {"input": "T", "answer": 30},
+        {"input": "BTA1MCNQL", "answer": 32},
+        {"input": "M3", "answer": 26},
+        {"input": "29", "answer": 12},
+        {"input": "HEEL", "answer": 23},
+        {"input": "33112", "answer": 6},
+        {"input": "707776957", "answer": 12},
+        {"input": "2648", "answer": 11},
+        {"input": "94308", "answer": 13},
+    ]
+}
